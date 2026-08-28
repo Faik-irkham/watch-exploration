99253dcba3b54ea810823ca86dde8eaf817b62fe
Faik-irkham <faikirkham@gmail.com>
2026-08-28 21:13:25 +0700

    Update widget test to use HeartRateApp instead of MyApp

--- hr_00_fondasi/test/widget_test.dart
+++ hr_00_fondasi/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:hr_00_fondasi/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HeartRateApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
