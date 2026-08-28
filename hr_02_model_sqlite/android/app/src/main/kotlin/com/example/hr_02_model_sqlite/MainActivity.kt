196ee800e20929e4c2facacea5bdf96fe39dbf33
Faik-irkham <faikirkham@gmail.com>
2026-08-28 22:05:27 +0700

    feat: Implement heart rate monitoring functionality with sensor integration

--- hr_02_model_sqlite/android/app/src/main/kotlin/com/example/hr_02_model_sqlite/MainActivity.kt
+++ hr_02_model_sqlite/android/app/src/main/kotlin/com/example/hr_02_model_sqlite/MainActivity.kt
@@ -1,5 +1,19 @@
 package com.example.hr_02_model_sqlite
 
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
+
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity(){
+  companion object {
+    private const val HEART_RATE_CHANNEL = "heart_rate/stream"
+  }
+
+  override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+    val messenger = flutterEngine.dartExecutor.binaryMessenger
+    EventChannel(messenger, HEART_RATE_CHANNEL)
+               .setStreamHandler(HeartRateStreamHandler(applicationContext))
+  }
+}
